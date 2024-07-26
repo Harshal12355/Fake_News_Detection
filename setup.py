77cdc21331bbf20981943efdae8cea09f5e77ff8
Harshal Thachapully <36196770+Harshal12355@users.noreply.github.com>
2024-07-26 14:44:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[],
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Harshal Shinoy Thachapully',
+    author_email='harshalts@gmail.com',
     description='A fake news detection project',
 )
